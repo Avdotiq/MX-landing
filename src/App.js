3de2c694d0ec91ebdb3c378c9c874f58575db4a7
Kristina Birukova <kbirukova18@gmail.com>
2024-01-25 12:44:50 +0100

    feat: creating basic structure
    creating pages, adding routing, adding helmet and creating basic settings, creating basic structure for components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Support from './pages/Support';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Head from './components/Head';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Head />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/score' element={<Support />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
