5edf2a47aedcd137be05c53d13adbc02220bccbe
Kristina Birukova <kbirukova18@gmail.com>
2024-01-26 19:21:11 +0100

    feat: adding general style, formatting

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,10 +2,12 @@ export const Colors = {
   primary: '#FFFFFF',
   primary_middle: '#DDDDDD;',
   primary_dark: '#000000',
+  primary_bg: '#201D27',
   secondary: '#888888',
   secondary_dark: '#0B0819',
   secondary_light: '#F4F4F4;',
+  btn_color: '#29A643',
   text: '#444444',
   warn: '#D51820',
-  success: '#3C763D',
+  success: '#3C763D'
 };
