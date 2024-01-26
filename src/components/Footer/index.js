fd51a355b545bdaf2e961f4f8e796ff41d452ed5
Kristina Birukova <kbirukova18@gmail.com>
2024-01-26 15:18:56 +0100

    feat: adding footer and header components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import Social from './Social';
+import Policy from './Policy';
+import { Breakpoint, Container } from '../../utils/grid';
+import { Colors } from '../../utils/theme';
 
 const Footer = () => {
   return (
-    <header>
-      <h1>Footer</h1>
-    </header>
+    <StyledFooter>
+      <Container>
+        <Social />
+        <Policy />
+      </Container>
+    </StyledFooter>
   );
 };
 
 export default Footer;
+
+const StyledFooter = styled.footer`
+  padding: 62px 0;
+  color: ${Colors.secondary};
+  background-color: ${Colors.primary_dark};
+
+  ${Breakpoint.lg} {
+    padding: 52px 0;
+  }
+
+  ${Breakpoint.lg} {
+    padding: 53px 0 64px;
+  }
+`;
