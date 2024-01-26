d54db128e388a1d83d720d1dd13215b2f2134a44
Kristina Birukova <kbirukova18@gmail.com>
2024-01-26 19:22:36 +0100

    fix: formatting

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 const Head = () => {
-    return (
-        <Helmet>
-            <title>XM member of traiding point group</title>
-            <meta name='description' content='Discover the power of trading with XM, a proud member of the Trading Point Group. Unlock endless possibilities by registering your account and accessing our cutting-edge trading platform.' />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Borel&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-        </Helmet>
-    );
+  return (
+    <Helmet>
+      <title>XM member of traiding point group</title>
+      <meta
+        name='description'
+        content='Discover the power of trading with XM, a proud member of the Trading Point Group. Unlock endless possibilities by registering your account and accessing our cutting-edge trading platform.'
+      />
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:wght@400;500;700&display=swap'
+        rel='stylesheet'
+      />
+    </Helmet>
+  );
 };
 
 export default Head;
